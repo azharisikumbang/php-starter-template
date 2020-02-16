b5f6699a40407537d8ef34169b13c4f9d85e8319
azharisaputrasikumbang@gmail.com <azharisaputrasikumbang@gmail.com>
2020-02-16 23:20:09 +0700

    config laravel-mix for js and css on resources folder

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('src/app.js', 'dist/').sass('src/app.scss', 'dist/');
+mix.js('app/resources/js/app.js', 'public/js/').sass('app/resources/css/app.scss', 'public/css/');
 
 // Full API
 // mix.js(src, output);
